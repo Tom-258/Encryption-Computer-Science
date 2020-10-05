0676fc1cc99d5344ba9c6437d99a4fb60d96ca7a
Thomas Stanger <thomasstanger0@gmail.com>
2020-10-05 10:31:59 +0100

    added

---
+++ CaesarCipher.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Encryption_Computer_Science
+{
+    public class CaesarCipher
+    {
+        private readonly int _key;
+        private readonly string _plainText;
+        private readonly string[] _alphabet = new[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}; 
+
+        public CaesarCipher(string plaintext, int shiftKey)
+        {
+            _plainText = plaintext;
+            _key = shiftKey;
+        }
+
+        public string Encrypt()
+        {
+            var encryptedList = new List<string>();
+            foreach (var letter in _plainText)
+            {
+                var stringLetter = letter.ToString();
+                if (Array.IndexOf(_alphabet, stringLetter) + _key >= 26)
+                {
+                    var modifiedLetter = _alphabet[Array.IndexOf(_alphabet, stringLetter) + _key - 26];
+                    encryptedList.Add(modifiedLetter);
+                }
+                else
+                {
+                    var modifiedLetter = _alphabet[Array.IndexOf(_alphabet, stringLetter) + _key];
+                    encryptedList.Add(modifiedLetter);
+                }
+            }
+
+            return string.Join("", encryptedList);
+        }
+
+        public string Decrypt(string message, int shift)
+        {
+            var decryptedList = new List<string>();
+            foreach (var letter in message)
+            {
+                var stringLetter = letter.ToString();
+                if (Array.IndexOf(_alphabet, stringLetter) - _key <= 0)
+                {
+                    var modifiedLetter = _alphabet[Array.IndexOf(_alphabet, stringLetter) - _key + 26];
+                    decryptedList.Add(modifiedLetter);
+                }
+                else
+                {
+                    var modifiedLetter = _alphabet[Array.IndexOf(_alphabet, stringLetter) - _key];
+                    decryptedList.Add(modifiedLetter);
+                }
+            }
+
+            return string.Join("", decryptedList);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,14 @@ namespace Encryption_Computer_Science
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Message:");
+            var message = Console.ReadLine();
+            Console.WriteLine("Shift:");
+            var shift = Convert.ToInt32(Console.ReadLine());
+            var caesarCipher = new CaesarCipher(message, shift);
+            var cypherText = caesarCipher.Encrypt();
+            Console.WriteLine(cypherText);
+            Console.WriteLine(caesarCipher.Decrypt(cypherText, shift));
         }
     }
 }
